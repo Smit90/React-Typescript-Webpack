b0f0748dab1574fdec1c72a03740feba99f5c195
Smit90 <45687440+Smit90@users.noreply.github.com>
2022-11-25 16:16:42 +0530

    added react-refresh plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './styles.css'
 import Image from './devops.png'
 import Laugh from './laughing.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
     return (
@@ -8,6 +9,7 @@ export const App = () => {
             <h1>React Typescript Webpack - {process.env.NODE_ENV}-{process.env.name}</h1>
             <img src={Image} alt="devops" />
             <img src={Laugh} alt="laughing" width="300" />
+            <ClickCounter />
         </>
     )
 }
