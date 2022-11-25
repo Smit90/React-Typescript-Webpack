e576e23c6d0a84e1e3429b354716c104c31ce2bb
Smit90 <45687440+Smit90@users.noreply.github.com>
2022-11-25 15:09:39 +0530

    configured multiple environment with webpack

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Laugh from './laughing.svg'
 export const App = () => {
     return (
         <>
-            <h1>React Typescript Webpack</h1>
+            <h1>React Typescript Webpack - {process.env.NODE_ENV}-{process.env.name}</h1>
             <img src={Image} alt="devops" />
             <img src={Laugh} alt="laughing" width="300" />
         </>
