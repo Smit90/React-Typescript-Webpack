e17bff1fc434f76db89758201377f72de4170f6f
Smit90 <45687440+Smit90@users.noreply.github.com>
2022-11-25 14:51:31 +0530

    handle css and images with webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import './styles.css'
+import Image from './devops.png'
+import Laugh from './laughing.svg'
+
 export const App = () => {
     return (
-        <h1>App</h1>
+        <>
+            <h1>React Typescript Webpack</h1>
+            <img src={Image} alt="devops" />
+            <img src={Laugh} alt="laughing" width="300" />
+        </>
     )
 }
